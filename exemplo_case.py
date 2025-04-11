4a7a0f3eb324438272b409867218ab9783a01e4f
Frankk Antonio <frankkantonio69@gmail.com>
2025-04-11 14:56:22 -0300

    Delete exemplo_case.py

--- exemplo_case.py
+++
@@ -1,8 +0,0 @@
-dia = int(input('Digite um dia númerico: '))
-match dia:
-  case 1:
-     print("Dia útil!")
-  case 2:
-   print("Final de semana ou feriado!")
-  case _:
-     print(f"Valor {dia} inválido")
