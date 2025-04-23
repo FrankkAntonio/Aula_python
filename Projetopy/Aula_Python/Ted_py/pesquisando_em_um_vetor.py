ed410d0c52565927f72ebe1b02186f3199493513
Frankk Antonio <frankkantonio69@gmail.com>
2025-04-23 20:51:23 -0300

    Add files via upload

--- Projetopy/Aula_Python/Ted_py/pesquisando_em_um_vetor.py
+++ Projetopy/Aula_Python/Ted_py/pesquisando_em_um_vetor.py
@@ -1,9 +1,30 @@
 from random import randint
 
-lista_numeros = []
+q = []
 
 for n in range(20):
     numero = randint(0, 1000)
-    lista_numeros.append(numero)
+    q.append(numero)
 
-print(lista_numeros)
+maior = -1
+indice_maior = -1
+menor = 1000
+indice_menor = -1
+
+for i in range(len(q)):
+    #teste se maior
+    if q[i] > maior:
+        maior = q[i]
+        indice_maior = i
+
+    #teste se menor
+    if q[i] < menor:
+        menor = q[i]
+        indice_menor = i
+
+print(q)
+print(f'O maior valor é {maior}')
+print(f'O indice do maior valor é {indice_maior}')
+print(f'O menor valor é {menor}')
+print(f'O indice do menor valor é {indice_menor}')
+
